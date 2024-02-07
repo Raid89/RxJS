54e1ff84c22cb3863eee81f410f0abccffca86b2
Raid89 <nicolas.cr0603@gmail.com>
2024-02-06 19:23:11 -0500

    se nura la funcionalidad del from y como me permite iterar sobre promesas y arrays

--- src/create.js
+++ src/create.js
@@ -1,7 +1,15 @@
 import { displayLog } from './utils';
-
+import { from } from 'rxjs';
 export default () => {
     /** start coding */
+
+    const myArray = [1,2,3,4,5];
+    const myString = "Hello World"
+    const myPromise = new Promise(resolve => setTimeout(() => {
+        resolve('Hello World')
+    }, 1000));
+    const observable = from(myPromise);
+    const subscription = observable.subscribe(val => displayLog(val));
 
     /** end coding */
 }
