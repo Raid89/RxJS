6718750d906ee6f00c68734d503ff65fffcb140e
Raid89 <nicolas.cr0603@gmail.com>
2024-02-06 19:49:49 -0500

    Se usa interval para mostrar datos en forma de intervalo y timer para ejecutar acciones tras un intervalo

--- src/create.js
+++ src/create.js
@@ -1,9 +1,17 @@
 import { displayLog } from './utils';
+import { interval, timer } from 'rxjs';
 
 export default () => {
     /** start coding */
 
+    const source = interval(500);
+    const subscription = source.subscribe(data => displayLog(data));
 
+    timer(3000).subscribe(() => subscription.unsubscribe());
 
+    const source2 = timer(4000, 100);
+    const subscription2 = source2.subscribe(data => displayLog(`2 - ${data}`))
+
+    timer(6000).subscribe(() => subscription2.unsubscribe());
     /** end coding */
 }
