a96532edf81bb4595b96dcb2d64180424dc04589
Raid89 <nicolas.cr0603@gmail.com>
2024-02-06 19:04:50 -0500

    Se crea funcion para imprimir en pantalla de forma asincrona usando Observable

--- src/create.js
+++ src/create.js
@@ -1,8 +1,17 @@
 import { displayLog } from './utils';
-
+import { Observable } from 'rxjs';
 export default () => {
     /** start coding */
 
+    const hello = Observable.create(function(observer){
+        observer.next("Hello");
+        setTimeout(() => {
+            observer.next("World");
+        }, 1000);
+        
+    });
+
+    const subscribe = hello.subscribe(event => displayLog(event));
 
 
     /** end coding */
